53c08952c67dc8feac1070861295c90a89d3d7cd
asapconet <aaronrickymeek@gmail.com>
2021-11-04 04:04:11 +0100

    installed all dependency and implimented basic design

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Reviews from "./reviews";
 function App() {
   return (
     <main>
-      <section>
         <div className='review-card'>
           <h1>Our reviews</h1>
           <div className='underlinings'></div>
           <Reviews />
         </div>
-      </section>
     </main>
   );
 }
