eabc3972e5cf577fcbf767be741f1b99749626e9
asapconet <aaronrickymeek@gmail.com>
2022-02-03 21:58:05 +0100

    reversion

--- src/review-data.jsx
+++ src/review-data.jsx
@@ -1,6 +1,6 @@
 const reviewers = [
   {
-      id:'2',
+    id: "2",
     name: "Tuedon Sticky",
     role: "Node Developer",
     info: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
@@ -8,21 +8,23 @@ const reviewers = [
       "https://media-exp1.licdn.com/dms/image/C5603AQFroI2udiUKCw/profile-displayphoto-shrink_200_200/0/1596801100154?e=1641427200&v=beta&t=DWMMQIzg62Jxo2PXVwIx3CWT4KCH2n5ga29BBkk_DE8",
   },
   {
-      id:'3',
+    id: "3",
     name: "Top Shit",
     role: "catching things",
     info: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
-    image: "https://media-exp1.licdn.com/dms/image/C4E03AQHCxnS6JnLrOQ/profile-displayphoto-shrink_200_200/0/1632297937010?e=1642032000&v=beta&t=HXYsD6SKzf3HoFVa9ePzaAZMNJahSEvYgSY8D5aCCuA",
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQHCxnS6JnLrOQ/profile-displayphoto-shrink_200_200/0/1632297937010?e=1642032000&v=beta&t=HXYsD6SKzf3HoFVa9ePzaAZMNJahSEvYgSY8D5aCCuA",
   },
   {
-      id:'2',
+    id: "2",
     name: "Musah Shayhi",
     role: "Full-Stack Developer",
     info: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    image: "https://media-exp1.licdn.com/dms/image/D4E35AQEOUxTGlEMo7g/profile-framedphoto-shrink_200_200/0/1628016720940?e=1636473600&v=beta&t=RwzKb951doby0nBlPwF9H_WQN4F41KxwD5PSQAk6Fp4",
+    image:
+      "https://media-exp1.licdn.com/dms/image/D4E35AQEOUxTGlEMo7g/profile-framedphoto-shrink_200_200/0/1628016720940?e=1636473600&v=beta&t=RwzKb951doby0nBlPwF9H_WQN4F41KxwD5PSQAk6Fp4",
   },
   {
-      id:'22',
+    id: "22",
     name: "Erons Owo",
     role: "Full-Stack Developer",
     info: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
@@ -30,7 +32,7 @@ const reviewers = [
       "https://media-exp1.licdn.com/dms/image/C4D03AQHM2Q_YAkVDRQ/profile-displayphoto-shrink_200_200/0/1617724068985?e=1641427200&v=beta&t=2X5FUe5eUwZEmqabR8hxqhN_yeN5btR--WFk2vTBKvY",
   },
   {
-      id:'4',
+    id: "4",
     name: "Lord-Sarcastic",
     role: "Full-Stack Developer",
     info: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
@@ -38,22 +40,13 @@ const reviewers = [
       "https://media-exp1.licdn.com/dms/image/C4E03AQEliE2_i9DhPw/profile-displayphoto-shrink_200_200/0/1623407112076?e=1641427200&v=beta&t=NKl0Svzu0QUm3FOJbxRp3C2733Z5WabZG3ANJBaCoy4",
   },
   {
-      id:'7',
+    id: "7",
     name: "ASAP_A1",
     role: "Eco Controler",
     info: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
     image:
       "https://media-exp1.licdn.com/dms/image/C4E03AQETtV_yHSl4hw/profile-displayphoto-shrink_200_200/0/1589279600462?e=1641427200&v=beta&t=u0QqExk54e21-G9xi-RJD9z3U31t_elPnJmVMj0herY",
-
   },
-  {
-      id:'7',
-    name: "ASAP_A1",
-    role: "Eco Controler",
-    info: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
-    image:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQETtV_yHSl4hw/profile-displayphoto-shrink_200_200/0/1589279600462?e=1641427200&v=beta&t=u0QqExk54e21-G9xi-RJD9z3U31t_elPnJmVMj0herY",
-  }
 ];
 
 export default reviewers;
